7fcc542bbb32f20b933d2bb648e31f8ade944910
annakom <ankakom3@gmail.com>
2019-02-25 12:09:18 -0500

    Cleaned up the project

--- project1.cpp
+++ project1.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-int main ()
+int main()
 {
 	vector<string> history1;
 	string		command;
@@ -15,85 +15,45 @@ int main ()
 		if(command.substr(0, 4) == "list")
 		{
 			if (command == "list")
-			{
-				//ft_add_history(history1, command);
 				system("ls");
-			}
 			else
 			{
-				/*DIR *directory;
-				struct dirent *file;
-
- 				directory = opendir( (command.substr(5, command.length())).c_str() );
-
- 				if(directory == NULL)
- 				{
-					cout << "***ERROR: INVALID PATH***" << endl;
-				}
-				else
-				{
-					cout << "DIRECTORY: " << command.substr(5, command.length()) << endl; 
-						while((file = readdir(directory)) != NULL)
-						{
-							cout << "\t" << file->d_name  << endl;
-						}
-					cout << "\n";
-				}*/
 				command = command.substr(5, command.length());
 				command = "ls " + command;
 				system(command.c_str());
 			}	
-		}			
+		}
+
 		else if(command == "history")
-		{
-			//ft_add_history(history1, command);
 			history(history1);
-		}
+
 		else if(command == "printwd")
-		{
-			//ft_add_history(history1, command);
 			printwd();
-		}
+
 		else if(command.substr(0, 5) == "chdir")
 		{
-			//ft_add_history(history1, command);
-			/*if(command.length() > 5 && command.substr(5, 1) == " "
-				&& chdir(command.substr(6, command.length()).c_str()) == 0 )
-			{
-				chdir(command.substr(6, command.length()).c_str());
-			}
+			if(command[5] != ' ')
+				cout << "Error!\n";
 			else
 			{
-				cout << "***ERROR: INVALID PATH***" << endl;
-			}*/
-			if(command[5] != ' '){
-				cout << "Error!\n";
-			}
-			else{
 				command = command.substr(6,command.length());
 				int isError = chdir(command.c_str());
-				if(isError){
-					cout << "***ERROR: INVALID PATH***\n";	
-				}
-			
+
+				if(isError)
+					cout <<"-bash: chdir: "<<command<< ": No such file or directory\n";		
 			}
 		}
-		else if(command != "\0")
-		{
-			system(command.c_str());
-		}	
-		/*else if (command == "exit")
-			exit(EXIT_SUCCESS);*/
 
-			cout << "UnknownShell#: " << GetCurrentWorkingDir() << ": ";
-			//cin >>command; 
-			getline(cin, command);
-			ft_add_history(history1, command);
+		else if(command != "\0")
+			system(command.c_str());
+
+	
+		cout << "UnknownShell#: " << GetCurrentWorkingDir() << ": ";
+		getline(cin, command);
+		ft_add_history(history1, command);
 
 	
 	}
-//	cout << "Current directory: " << GetCurrentWorkingDir() << endl;
-	//}
 	return 0;
 }
 
